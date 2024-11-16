a8331f14812a476420ad6eea42346a212dd5e67c
Rob <160157653+RobLaran@users.noreply.github.com>
2024-11-16 23:35:21 +0800

    final commit to publish the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,2 @@
-module org.cms.contactmanagementsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-    requires java.sql;
-    requires mysql.connector.j;
-
-    opens org.cms.contactmanagementsystem to javafx.fxml;
-    exports org.cms.contactmanagementsystem;
+module $MODULE_NAME$ {
 }
---
+++ src/main/java/org/cms/contactmanagementsystem/Main.java
@@ -0,0 +1,2 @@
+package org.cms.contactmanagementsystem;public class Main {
+}
